f3b67ac5c07269ec4a6a22e5b893e005becda188
erendemirel <dmrleren@gmail.com>
2022-12-18 19:47:47 +0100

    Add support for update

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
     return result;
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
@@ -32,6 +44,8 @@ const QUESTIONS = [
     }
 ];
 const CURR_DIR = process.cwd();
+let isUpdate = false;
+let options;
 inquirer.prompt(QUESTIONS).then(answers => {
     const projectChoice = answers['template'];
     const projectName = answers['name'];
@@ -39,7 +53,7 @@ inquirer.prompt(QUESTIONS).then(answers => {
     const templatePath = path.join(__dirname, 'templates', projectChoice);
     //@ts-ignore
     const tartgetPath = path.join(CURR_DIR, projectName);
-    const options = {
+    options = {
         //@ts-ignore
         projectName,
         //@ts-ignore
@@ -47,20 +61,20 @@ inquirer.prompt(QUESTIONS).then(answers => {
         templatePath,
         tartgetPath
     };
-    if (!createProject(tartgetPath)) {
-        return;
-    }
+    createProject(tartgetPath);
     //@ts-ignore
     createDirectoryContents(templatePath, projectName);
     postProcess(options);
 });
 function createProject(projectPath) {
     if (fs.existsSync(projectPath)) {
-        console.log(chalk_1.default.red(`Folder ${projectPath} exists. Delete or use another name.`));
-        return false;
+        console.log(chalk_1.default.yellowBright('Updating the project..'));
+        isUpdate = true;
     }
-    fs.mkdirSync(projectPath);
-    return true;
+    else if (!fs.existsSync(projectPath)) {
+        console.log(chalk_1.default.yellowBright('Creating the project..'));
+        fs.mkdirSync(projectPath);
+    }
 }
 const SKIP_FILES = ['node_modules', '.template.json'];
 function createDirectoryContents(templatePath, projectName) {
@@ -83,8 +97,10 @@ function createDirectoryContents(templatePath, projectName) {
             fs.writeFileSync(writePath, contents, 'utf8');
         }
         else if (stats.isDirectory()) {
-            // create folder in destination folder
-            fs.mkdirSync(path.join(CURR_DIR, projectName, file));
+            if (!fs.existsSync(path.join(CURR_DIR, projectName, file))) {
+                // create folder in destination folder
+                fs.mkdirSync(path.join(CURR_DIR, projectName, file));
+            }
             // copy files/folder inside current folder recursively
             createDirectoryContents(path.join(templatePath, file), path.join(projectName, file));
         }
@@ -96,8 +112,15 @@ function postProcess(options) {
         shell.cd(options.tartgetPath);
         const result = shell.exec('npm install');
         if (result.code !== 0) {
+            console.log(chalk_1.default.redBright('Failed'));
             return false;
         }
+    }
+    if (isUpdate) {
+        console.log(chalk_1.default.greenBright('Project successfully updated'));
+    }
+    else {
+        console.log(chalk_1.default.greenBright('Project successfully created'));
     }
     return true;
 }
--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,17 @@ import * as shell from 'shelljs';
 
 const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
 const QUESTIONS = [
-{
-    name: 'template',
-    type: 'list',
-    message: 'What template would you like to use?',
-    choices: CHOICES
-},
-{
-    name: 'name',
-    type: 'input',
-    message: 'Please input a new project name:'
-}];
+    {
+        name: 'template',
+        type: 'list',
+        message: 'What template would you like to use?',
+        choices: CHOICES
+    },
+    {
+        name: 'name',
+        type: 'input',
+        message: 'Please input a new project name:'
+    }];
 
 export interface CliOptions {
     projectName: string
@@ -30,6 +30,10 @@ export interface CliOptions {
 
 const CURR_DIR = process.cwd();
 
+let isUpdate: Boolean = false;
+
+let options: CliOptions;
+
 inquirer.prompt(QUESTIONS).then(answers => {
     const projectChoice = answers['template'];
     const projectName = answers['name'];
@@ -37,8 +41,8 @@ inquirer.prompt(QUESTIONS).then(answers => {
     const templatePath = path.join(__dirname, 'templates', projectChoice);
     //@ts-ignore
     const tartgetPath = path.join(CURR_DIR, projectName);
-    
-    const options: CliOptions = {
+
+    options = {
         //@ts-ignore
         projectName,
         //@ts-ignore
@@ -47,9 +51,7 @@ inquirer.prompt(QUESTIONS).then(answers => {
         tartgetPath
     }
 
-    if (!createProject(tartgetPath)) {
-        return;
-    }
+    createProject(tartgetPath);
 
     //@ts-ignore
     createDirectoryContents(templatePath, projectName);
@@ -59,12 +61,12 @@ inquirer.prompt(QUESTIONS).then(answers => {
 
 function createProject(projectPath: string) {
     if (fs.existsSync(projectPath)) {
-        console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`));
-        return false;
+        console.log(chalk.yellowBright('Updating the project..'));
+        isUpdate = true;
+    } else if (!fs.existsSync(projectPath)) {
+        console.log(chalk.yellowBright('Creating the project..'));
+        fs.mkdirSync(projectPath);
     }
-    fs.mkdirSync(projectPath);
-    
-    return true;
 }
 
 const SKIP_FILES = ['node_modules', '.template.json'];
@@ -75,23 +77,25 @@ function createDirectoryContents(templatePath: string, projectName: string) {
     // loop each file/folder
     filesToCreate.forEach(file => {
         const origFilePath = path.join(templatePath, file);
-        
+
         // get stats about the current file
         const stats = fs.statSync(origFilePath);
-    
+
         // skip files that should not be copied
         if (SKIP_FILES.indexOf(file) > -1) return;
-        
+
         if (stats.isFile()) {
             // read file content and transform it using template engine
             let contents = fs.readFileSync(origFilePath, 'utf8');
-            contents = template.render(contents, { projectName });
+            contents = template.render(contents, {projectName});
             // write file to destination folder
             const writePath = path.join(CURR_DIR, projectName, file);
             fs.writeFileSync(writePath, contents, 'utf8');
         } else if (stats.isDirectory()) {
-            // create folder in destination folder
-            fs.mkdirSync(path.join(CURR_DIR, projectName, file));
+            if (!fs.existsSync(path.join(CURR_DIR, projectName, file))) {
+                // create folder in destination folder
+                fs.mkdirSync(path.join(CURR_DIR, projectName, file));
+            }
             // copy files/folder inside current folder recursively
             createDirectoryContents(path.join(templatePath, file), path.join(projectName, file));
         }
@@ -104,10 +108,17 @@ function postProcess(options: CliOptions) {
         shell.cd(options.tartgetPath);
         const result = shell.exec('npm install');
         if (result.code !== 0) {
+            console.log(chalk.redBright('Failed'));
             return false;
         }
     }
-    
+
+    if (isUpdate) {
+        console.log(chalk.greenBright('Project successfully updated'));
+    } else {
+        console.log(chalk.greenBright('Project successfully created'));
+    }
+
     return true;
 }
 
